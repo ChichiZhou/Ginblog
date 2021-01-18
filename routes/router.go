5d35221171a9adfd5c4dbba20f860fa02669e7ee
wejectchan <weject@gmail.com>
2021-01-18 09:33:40 +0800

    更改后端托管

--- routes/router.go
+++ routes/router.go
@@ -4,36 +4,35 @@ import (
 	"ginblog/api/v1"
 	"ginblog/middleware"
 	"ginblog/utils"
-	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
-func createMyRender() multitemplate.Renderer {
-	p := multitemplate.NewRenderer()
-	p.AddFromFiles("admin", "static/admin/index.html")
-	p.AddFromFiles("front", "static/front/index.html")
-	return p
-}
+//func createMyRender() multitemplate.Renderer {
+//	p := multitemplate.NewRenderer()
+//	p.AddFromFiles("admin", "static/admin/index.html")
+//	p.AddFromFiles("front", "static/front/index.html")
+//	return p
+//}
 
 func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.New()
-	r.HTMLRender = createMyRender()
+	//r.HTMLRender = createMyRender()
 	r.Use(middleware.Log())
 	r.Use(gin.Recovery())
 	r.Use(middleware.Cors())
 
-	r.Static("/static", "./static/front/static")
-	r.Static("/admin", "./static/admin")
-	r.StaticFile("/favicon.ico", "static/front/favicon.ico")
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "front", nil)
-	})
-
-	r.GET("/admin", func(c *gin.Context) {
-		c.HTML(200, "admin", nil)
-	})
+	//r.Static("/static", "./static/front/static")
+	//r.Static("/admin", "./static/admin")
+	//r.StaticFile("/favicon.ico", "static/front/favicon.ico")
+	//
+	//r.GET("/", func(c *gin.Context) {
+	//	c.HTML(200, "front", nil)
+	//})
+	//
+	//r.GET("/admin", func(c *gin.Context) {
+	//	c.HTML(200, "admin", nil)
+	//})
 
 	auth := r.Group("api/v1")
 	auth.Use(middleware.JwtToken())
